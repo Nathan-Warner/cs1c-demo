d6dcc96155e644b1e193e83aa545706bf80bae50
Nathan Warner <nwarner4@saddleback.edu>
2022-10-10 20:40:25 -0700

    Add an array of numbers

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main() {
 	cout << "Hello World!" << endl;
 
-	cout << add(5, 3);
+	cout << add(5, 3) << endl;
+
+	int numbers[] = {1, 2, 3, 4};
+	cout << add(numbers, 4) << endl;
 	
 	return 0;
 }
