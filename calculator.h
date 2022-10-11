22299aadd13d1e4e042cf798db2933c22ac9b5ca
Nathan-Warner <nwarner4@saddleback.edu>
2022-10-10 20:47:21 -0700

    Nathan's add feature (#1)
    * Basic add functionality
    
    * Add an array of numbers

---
+++ calculator.h
@@ -0,0 +1,7 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int add(int first, int second);
+int add(int numbers[], int count);
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main() {
 	cout << "Hello World!" << endl;
+
+	cout << add(5, 3) << endl;
+
+	int numbers[] = {1, 2, 3, 4};
+	cout << add(numbers, 4) << endl;
 	
 	return 0;
 }
